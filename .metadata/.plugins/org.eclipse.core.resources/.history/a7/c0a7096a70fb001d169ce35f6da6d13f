4760428c5560478ec322d5e5ed77a4009861182a
Akmal Dzaky H <akmaldzaky33@gmail.com>
2023-05-26 18:43:39 +0700

    CP Tugas 1

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a7/c0a7096a70fb001d169ce35f6da6d13f
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author akmal
+ *
+ */
+module Tugas {
+}
---
+++ Tugas/src/tugasPertama/Soal_3.java
@@ -0,0 +1,24 @@
+package tugasPertama;
+import java.util.*;
+
+public class Soal_3 {
+
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.print("Masukkan angka: ");
+			int n = input.nextInt(); // Jumlah baris pola Fibonacci
+			for (int i = 0; i < n; i++) {
+				System.out.print(tribonacci(i) + " ");
+			}
+		}
+
+	}
+
+	public static int tribonacci(int n) {
+		if (n == 0 || n == 1 || n == 2)
+			return 1;
+		else
+			return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+	}
+
+}
